e6cd3414482b1f9135160788bf7c27996c67bd6b
Mark Scannell <mfscannell@users.noreply.github.com>
2025-04-05 16:59:43 -0500

    KCSDK-1001: Added GetPasswordRequirements

--- KofCSDK/IKofCV1Client.cs
+++ KofCSDK/IKofCV1Client.cs
@@ -7,5 +7,6 @@ namespace KofCSDK;
 public interface IKofCV1Client
 {
     Task<Result<LoginResponse>> LoginAsync(TenantInfo tenantInfo, LoginRequest request, CancellationToken cancellationToken = default);
+    Task<Result<PasswordRequirements>> GetPasswordRequirementsAsync(TenantInfo tenantInfo, UserAuthentication userAuthentication, CancellationToken cancellationToken = default);
     Task<Result<List<Activity>>> GetAllActivities(TenantInfo tenantInfo, UserAuthentication userAuthentication, CancellationToken cancellationToken = default);
 }
